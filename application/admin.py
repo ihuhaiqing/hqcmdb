dff647cfe620b7b49dc5aa927c577b847fd8e669
huhaiqng <1321385590@qq.com>
2024-08-29 17:56:03 +0800

    update

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from application.models import MySQL
 
-# Register your models here.
+@admin.register(MySQL)
+class MySQLAdmin(admin.ModelAdmin):
+    list_display = ('name', 'host', 'port', 'user', 'get_plain_password', 'database')
+    search_fields = ('name', 'host__hostname', 'database')
+    list_filter = ('host',)
+
+    def get_plain_password(self, obj):
+        return obj.password
+    get_plain_password.short_description = '明文密码'
