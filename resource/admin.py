bb6e2436bfee228cafd57a693b7910ccea73e08f
huhaiqng <1321385590@qq.com>
2024-08-30 14:53:24 +0800

    host os

--- resource/admin.py
+++ resource/admin.py
@@ -7,6 +7,7 @@ class HostAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'ip_address', 'operating_system', 'cpu_cores', 'memory_gb', 'storage_gb', 'description', 'created_at', 'updated_at')
     search_fields = ('hostname', 'ip_address', 'operating_system')
     list_filter = ('operating_system', 'cpu_cores')
+    list_per_page = 20
 
 # 也可以使用简单的注册方式
 # admin.site.register(Host, HostAdmin)
