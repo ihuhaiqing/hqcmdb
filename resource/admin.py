93be1f92d48f552b9fa5617025af4c1c148558a8
huhaiqng <1321385590@qq.com>
2024-09-03 11:51:17 +0800

    commit

--- resource/admin.py
+++ resource/admin.py
@@ -2,14 +2,6 @@ from django import forms
 from django.contrib import admin
 from .models import Host
 
-class HostForm(forms.ModelForm):
-    class Meta:
-        model = Host
-        fields = '__all__'
-        widgets = {
-            'projects': forms.CheckboxSelectMultiple,  # 使用复选框显示多对多字段
-        }
-
 # 注册主机模型
 @admin.register(Host)
 class HostAdmin(admin.ModelAdmin):
@@ -17,7 +9,6 @@ class HostAdmin(admin.ModelAdmin):
     search_fields = ('ip_address',)
     list_filter = ('operating_system', 'environment')
     list_per_page = 20
-    form = HostForm
 
     def display_projects(self, obj):
         return ", ".join([project.name for project in obj.projects.all()])
