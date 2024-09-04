6eae92bc605bd74ca7fa82ae995becdf7ca914cd
huhaiqng <1321385590@qq.com>
2024-09-04 17:20:05 +0800

    modified:   application/models.py

--- application/models.py
+++ application/models.py
@@ -11,7 +11,7 @@ class MySQL(models.Model):
     role = models.CharField(max_length=50, verbose_name="角色", choices=[('PRIMARY', 'PRIMARY'), ('SECONDARY', 'SECONDARY')], default='PRIMARY')
     username = models.CharField(max_length=255, verbose_name="用户名", default="root")
     password = EncryptedCharField(max_length=255, verbose_name="密码", default="password")
-    version = models.CharField(max_length=50, verbose_name="MySQL 版本号", default="8.0")
+    version = models.CharField(max_length=50, verbose_name="版本号", default="8.0")
     project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="项目")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
