7004dfa3636845bd61db86d62471b5c120e08467
huhaiqng <1321385590@qq.com>
2024-09-20 15:06:40 +0800

    token

--- resource/views.py
+++ resource/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Host
 from .serializers import HostSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # for promethues host targets discovery
 class HostTargetsViewSet(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         items = Host.objects.all()
         serializer = HostSerializer(items, many=True)
